c2dc1e43f7c147215b39e3d2faa2b81adc5ebcf1
Javier Moreno <bluelephant@gmail.com>
2018-06-12 21:27:00 -0400

    Scrapper for extracting preliminaries scores plus feather file with the extracted table.

---
+++ scrapping/scrapper.R
@@ -0,0 +1,32 @@
+library(rvest)
+library(stringr)   
+library(lubridate)
+library(purrr)
+library(dplyr)
+
+extract_result_data <- function(result_node) {
+  date <- result_node %>% html_node('.mu-i-date') %>% html_text() %>% dmy()
+  
+  home_team <- result_node %>% html_node('.t.home') %>% html_node('.t-nText') %>% html_text()  
+  away_team <- result_node %>% html_node('.t.away') %>% html_node('.t-nText') %>% html_text() 
+  score_text <- result_node %>% html_node('.s-scoreText') %>% html_text() %>% str_split('-') %>% unlist() %>% as.numeric()
+  home_goals <- score_text[1]
+  away_goals <- score_text[2]
+  
+  return(list(date=date, home_team=home_team, away_team=away_team, 
+              home_goals=home_goals, away_goals=away_goals))
+}
+
+get_results <- function(url){
+  url %>% read_html() %>% html_nodes('.mu.result') %>% map(extract_result_data) %>% bind_rows()
+}
+
+urls <- c('https://www.fifa.com/worldcup/preliminaries/southamerica/all-matches.html', 
+          'https://www.fifa.com/worldcup/preliminaries/europe/all-matches.html', 
+          'https://www.fifa.com/worldcup/preliminaries/nccamerica/all-matches.html', 
+          'https://www.fifa.com/worldcup/preliminaries/africa/all-matches.html',
+          'https://www.fifa.com/worldcup/preliminaries/asia/all-matches.html',
+          'https://www.fifa.com/worldcup/preliminaries/oceania/all-matches.html')
+
+results <- urls %>% map(get_results) %>% bind_rows()
+
